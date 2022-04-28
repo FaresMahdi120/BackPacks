567cbe86e5871d5cabc0929b53a80a3c66d5d8fa
fares mahdi <hhfares305@gmail.com>
2022-04-28 18:44:48 +0100

    Fixed the Path not being the same and returning a null value.

--- src/main/java/dev/maahdi/Items/BackPackItems.java
+++ src/main/java/dev/maahdi/Items/BackPackItems.java
@@ -23,9 +23,9 @@ public class BackPackItems {
             case "basic":
                 return itemsUtil.CreateItem((String) plugin.lang.get_Value("BackPacks.Basic.name-spaced-key"),Material.CHEST, getLangFile().getString("BackPacks.Basic.name"), 1, plugin.getLangConfig().getStringList("BackPacks.Basic.lore") , true, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
             case "advanced":
-                return itemsUtil.CreateItem((String) plugin.lang.get_Value("BackPacks.Basic.name-spaced-key"),Material.CHEST, getLangFile().getString("BackPacks.Advanced.name"), 1, plugin.getLangConfig().getStringList("BackPacks.Advanced.lore") , true, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
+                return itemsUtil.CreateItem((String) plugin.lang.get_Value("BackPacks.Advanced.name-spaced-key"),Material.CHEST, getLangFile().getString("BackPacks.Advanced.name"), 1, plugin.getLangConfig().getStringList("BackPacks.Advanced.lore") , true, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
             case "extra":
-                return itemsUtil.CreateItem((String) plugin.lang.get_Value("BackPacks.Basic.name-spaced-key"),Material.CHEST, getLangFile().getString("BackPacks.Extra.name"), 1, plugin.getLangConfig().getStringList("BackPacks.Extra.lore") , true, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
+                return itemsUtil.CreateItem((String) plugin.lang.get_Value("BackPacks.Extra.name-spaced-key"),Material.CHEST, getLangFile().getString("BackPacks.Extra.name"), 1, plugin.getLangConfig().getStringList("BackPacks.Extra.lore") , true, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
         }
         return null;
     }
