01eccde64f57dd8b404f47008a9a38efec216376
FaresMahdi <hhfares305@gmail.com>
2022-12-31 19:28:03 +0100

    Revamped Files (aka deleted)

---
+++ src/main/java/dev/FaresMahdi120/BackPacks.java
@@ -0,0 +1,15 @@
+package dev.FaresMahdi120;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class BackPacks extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
--- src/main/java/dev/maahdi/Commands/PlayerCommands.java
+++
@@ -1,4 +0,0 @@
-package dev.maahdi.Commands;
-
-public class PlayerCommands {
-}
