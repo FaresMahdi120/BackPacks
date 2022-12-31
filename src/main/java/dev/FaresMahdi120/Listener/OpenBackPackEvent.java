f7ccf7f8388ae27ee567e5a3e8a1c0fd0898bb9e
FaresMahdi <hhfares305@gmail.com>
2022-12-31 19:31:39 +0100

    A listener for the InventoryOpenEvent

---
+++ src/main/java/dev/FaresMahdi120/Listener/OpenBackPackEvent.java
@@ -0,0 +1,13 @@
+package dev.FaresMahdi120.Listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+
+public class OpenBackPackEvent implements Listener {
+
+    @EventHandler
+    public void onBackPackOpenEvent(InventoryOpenEvent e){
+
+    }
+}
